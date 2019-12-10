179a5a2262d6cb24a974a1937579a2bd418b94da
Matteo Lucas <matteo.lucas04@gmail.com>
2019-12-10 19:32:35 +0100

    Update Meteo.py

--- Meteo.py
+++ Meteo.py
@@ -1,15 +1,15 @@
 import requests
 from googletrans import Translator
 
-
+lieu = "Rennes"
 ### On recupere les informations de meteo du jours
-url="http://api.openweathermap.org/data/2.5/weather?q=Tinteniac,fr&appid=13e83287d3960d70d1a644490c1662c0&units=metric"
+url="http://api.openweathermap.org/data/2.5/weather?q=" + lieu + ",fr&appid=13e83287d3960d70d1a644490c1662c0&units=metric"
 content = requests.get(url)
 data=content.json()
 t = data["main"]['temp']
 d = data["weather"][0]['description']
 meteo = "The temperature is " + str(round(t)) + " degree celsius and there is " + str(d)
-
+print(meteo)
 ### on traduit les informations de meteo
 translator = Translator()
 traduced = (translator.translate(meteo, src='en', dest='fr')).text + ' '
