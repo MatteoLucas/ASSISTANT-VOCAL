c2d517164e764a100ed11b1f72c3d8ad03582a5a
Matteo Lucas <matteo.lucas04@gmail.com>
2019-12-10 22:28:40 +0100

    Update

--- Meteo.py
+++ Meteo.py
@@ -1,25 +1,59 @@
 import requests
-from googletrans import Translator
+import win32com.client
+import os
+import speech_recognition as sr
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
-lieu = "Rennes"
-### On recupere les informations de meteo du jours
-url="http://api.openweathermap.org/data/2.5/weather?q=" + lieu + ",fr&appid=13e83287d3960d70d1a644490c1662c0&units=metric&lang=fr"
-content = requests.get(url)
-data=content.json()
-t = data["main"]['temp']
-d = data["weather"][0]['icon']
+def meteoDuJour(lieu):
+    ### On recupere les informations de meteo du jours
+    url="http://api.openweathermap.org/data/2.5/weather?q=" + lieu + ",fr&appid=13e83287d3960d70d1a644490c1662c0&units=metric"
+    content = requests.get(url)
+    data=content.json()
+    try :
+        t = data["main"]['temp']
+        d = data["weather"][0]['icon']
 
-### On s'occupe de la temperature
-temp = "La temperature est de " + str(round(t)) + " degrés"
+        ### On s'occupe de la temperature
+        temp = "La température est de " + str(round(t)) + " degrés"
 
-### On s'occupe de l'etat du ciel
-ciel = {'01': 'le ciel est degagé', '02': 'Il y a quelques nuages',  '03': 'Le ciel est nuageux', '04': 'Le ciel est tres nuageux', '09': 'Il y a de fortes pluies', '10': 'Il pleut',  '11': "Il y a de l'orage", '13': 'Il neige', '50': 'Il y a du brouillard'}
-if d[2] == 'n':
-    meteo = 'Il fait nuit, ' + ciel[d[0:2]]
-else :
-    meteo = ciel[d[0:2]]
+        ### On s'occupe de l'etat du ciel
+        ciel = {'01': 'le ciel est degagé', '02': 'Il y a quelques nuages',  '03': 'Le ciel est nuageux', '04': 'Le ciel est très nuageux', '09': 'Il y a de fortes pluies', '10': 'Il pleut',  '11': "Il y a de l'orage", '13': 'Il neige', '50': 'Il y a du brouillard'}
+        if d[2] == 'n':
+            meteo = 'Il fait nuit, ' + ciel[d[0:2]]
+        else :
+            meteo = ciel[d[0:2]]
 
-### On affiche la meteo
-print(temp)
-print(meteo)
+        ### On affiche la meteo
+        print(temp)
+        print(meteo)
+        speaker.Speak(temp)
+        speaker.Speak(meteo)
+    except KeyError:
+        speaker.Speak('lieu introuvable')
+        os.system('python Meteo.py')
+
+
+def connaitreLieu() :
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            speaker.Speak("De quel lieu voulez-vous connaitre la météo ?")
+            audio = r.listen(source)
+        except sr.UnknownValueError and sr.RequestError as e:
+            print('')
+    ecoute = r.recognize_google(audio, language="fr-FR")
+    longueur = len(ecoute)
+    lieu = ''
+    for caractere in range(0, longueur):
+        lettre = ord(ecoute[caractere])
+        if lettre == 32:
+            nouvelleLettre = 45
+        else:
+            nouvelleLettre = lettre
+        lieu += chr(nouvelleLettre)
+    meteoDuJour(lieu)
+
+
+
+connaitreLieu()
 
--- Reconnaisance vocale.py
+++ Reconnaisance vocale.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
-
+import os
+import win32com.client
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
 def ecouteContinue() :
     r = sr.Recognizer()
@@ -18,11 +20,19 @@ def ecouteContinue() :
 
 def ecoutePrecise() :
     r = sr.Recognizer()
+    global tache
     with sr.Microphone() as source :
-        print("Dites quelque chose")
-        audio = r.listen(source)
+        try :
+            print("Je vous écoute")
+            speaker.Speak("Je vous écoute")
+            audio = r.listen(source)
+        except sr.UnknownValueError and sr.RequestError as e:
+            print('')
     tache = r.recognize_google(audio, language="fr-FR")
     print("vous avez dit : ", tache)
 
-tache = ''
+
 ecouteContinue()
+meteo = tache.find("météo")
+if meteo != -1 :
+    os.system('python Meteo.py')
